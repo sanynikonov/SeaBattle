9514d90d984b98e41d04d4b2380062a0a995fb0b
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-10-29 12:11:11 +0200

    Creates field director for classic field settings

---
+++ SeaBattle.Domain/Fields/FieldDirector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeaBattle.Domain
+{
+    public class FieldDirector
+    {
+        public void BuildClassicField(IFieldBuilder fieldBuilder)
+        {
+            var dimension = 10;
+
+            var storage = new Dictionary<ShipType, int>
+            {
+                { ShipType.SingleDeck, 4 },
+                { ShipType.DoubleDeck, 3 },
+                { ShipType.ThreeDeck, 2 },
+                { ShipType.FourDeck, 1 }
+            };
+
+            fieldBuilder.SetDimension(dimension);
+            fieldBuilder.SetShipsStorage(storage);
+        }
+    }
+}
