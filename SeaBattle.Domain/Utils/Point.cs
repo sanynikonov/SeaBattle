d9d3c1aec1d594532a3774d48536c374178388a5
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-10-30 21:43:52 +0200

    Overrides equlaity check in Point

--- SeaBattle.Domain/Utils/Point.cs
+++ SeaBattle.Domain/Utils/Point.cs
@@ -15,5 +15,25 @@ namespace SeaBattle.Domain
             X = x;
             Y = y;
         }
+
+        public static bool operator ==(Point first, Point second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Point first, Point second)
+        {
+            return !first.Equals(second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point point))
+            {
+                return false;
+            }
+            
+            return X == point.X && Y == point.Y;
+        }
     }
 }
