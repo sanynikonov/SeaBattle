54efcfa4712e144cbe3942c4bf727758314f2799
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-11-01 16:09:18 +0200

    Creates console client object

---
+++ SeaBattle.Client.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeaBattle.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
