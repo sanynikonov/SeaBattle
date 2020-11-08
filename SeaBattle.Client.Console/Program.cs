978209554d5569ec8a656f6c6ad419f56cbdabba
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-11-08 12:27:58 +0200

    Fills program with test data

--- SeaBattle.Client.Console/Program.cs
+++ SeaBattle.Client.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeaBattle.Domain;
+using System.Collections.Generic;
 
 namespace SeaBattle.Client
 {
@@ -6,7 +7,41 @@ namespace SeaBattle.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fieldBuilder = new FieldBuilder();
+
+            fieldBuilder.SetDimension(5);
+            fieldBuilder.SetShipsStorage(new Dictionary<ShipType, int> { { ShipType.SingleDeck, 2 } });
+            fieldBuilder.SetShip(new Point(0, 0), new Point(0, 0));
+            fieldBuilder.SetShip(new Point(4, 4), new Point(4, 4));
+
+            var firstField = fieldBuilder.Result;
+
+            fieldBuilder.Reset();
+
+            fieldBuilder.SetDimension(5);
+            fieldBuilder.SetShipsStorage(new Dictionary<ShipType, int> { { ShipType.SingleDeck, 2 } });
+            fieldBuilder.SetShip(new Point(0, 0), new Point(0, 0));
+            fieldBuilder.SetShip(new Point(4, 4), new Point(4, 4));
+
+            var secondField = fieldBuilder.Result;
+
+            var gameInfo = new GameStartInfo(
+                new Player { Name = "Kostia" }, 
+                new Player { Name = "Voronin" }, 
+                firstField, 
+                secondField);
+
+            //var preparationView = new GamePreparationView(fieldBuilder);
+
+            //var gameInfo = preparationView.Run();
+
+            var gameFactory = new ConsoleGameFactory();
+
+            var game = gameFactory.CreateGame(gameInfo);
+
+            var gameView = new ConsoleGameView(game);
+
+            gameView.Run();
         }
     }
 }
