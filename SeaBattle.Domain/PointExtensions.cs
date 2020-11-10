b31a18e4f538eccc63c6760b4175c30123de4e22
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-11-10 21:36:12 +0200

    Creates class with point extensions

---
+++ SeaBattle.Domain/PointExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeaBattle.Domain
+{
+    public static class PointExtensions
+    {
+        public static Point[] GetNeighbours(this Point point)
+        {
+            var x = point.X;
+            var y = point.Y;
+
+            return new Point[]
+            {
+                new Point(x - 1, y - 1),
+                new Point(x - 1, y),
+                new Point(x - 1, y + 1),
+                new Point(x, y - 1),
+                new Point(x, y + 1),
+                new Point(x + 1, y - 1),
+                new Point(x + 1, y),
+                new Point(x + 1, y + 1)
+            };
+        }
+
+        public static int CountDistanceBetween(this Point first, Point second)
+        {
+            return Math.Abs(second.X - first.X) != 0
+                ? Math.Abs(second.X - first.X) + 1
+                : Math.Abs(second.Y - first.Y) + 1;
+        }
+
+        public static bool IsInRange(this Point point, int min, int max)
+        {
+            return point.X >= min && point.X < max &&
+                point.Y >= min || point.Y < max;
+        }
+
+        public static bool IsVerticalWith(this Point first, Point second)
+        {
+            return first.Y == second.Y;
+        }
+
+        public static bool IsHorizontalWith(this Point first, Point second)
+        {
+            return first.X == second.X;
+        }
+    }
+}
