a51ebd3ae0fe2499027cbbfec9fde5c1b0da8f08
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-11-18 00:47:05 +0200

    Creates tests for setting ships randomly

--- SeaBattle.Domain.Tests/Fields/FieldDirectorTest.cs
+++ SeaBattle.Domain.Tests/Fields/FieldDirectorTest.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -56,6 +57,49 @@ namespace SeaBattle.Domain.Tests.Fields
             void action() => _fieldDirector.SetShipsRandomly(fieldBuilder);
 
             Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void SetShipsRandomly_WithCorrectBuilder_SetsShipsFromPresetShipsStorage()
+        {
+            var fieldBuilder = new FieldBuilder();
+            var shipsStorage = new Dictionary<ShipType, int> { { ShipType.SingleDeck, 1 }, { ShipType.DoubleDeck, 1 } };
+            var expected = 3;
+
+            fieldBuilder.SetDimension(5);
+            fieldBuilder.SetShipsStorage(shipsStorage);
+
+            _fieldDirector.SetShipsRandomly(fieldBuilder);
+            var actual = fieldBuilder.Result.Cells.Cast<Cell>().Count(c => c.HasDeck);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SetShipsRandomly_WithCorrectBuilder_SetShipsHaveFreeSpaceAround()
+        {
+            var dimension = 5;
+            var fieldBuilder = new FieldBuilder();
+            fieldBuilder.SetDimension(dimension);
+            var shipsStorage = new Dictionary<ShipType, int> { { ShipType.SingleDeck, 3 } };
+            fieldBuilder.SetShipsStorage(shipsStorage);
+
+            _fieldDirector.SetShipsRandomly(fieldBuilder);
+
+            var cells = fieldBuilder.Result.Cells.Cast<Cell>();
+
+            var coordinates = cells.Where(c => c.HasDeck).Select(c => c.Coordinates);
+
+            foreach (var cellCoordinates in coordinates)
+            {
+                var neighbourCellsHaveDecks = cellCoordinates
+                    .GetNeighbours()
+                    .Where(p => p.IsInRange(0, dimension))
+                    .Select(p => fieldBuilder.Result.Cells[p.X, p.Y])
+                    .All(c => c.HasDeck);
+
+                Assert.False(neighbourCellsHaveDecks);
+            }
         }
     }
 }
